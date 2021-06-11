c98d27e2b19eb040c25aa2993c512f8f35b00712
Brian Mugo <brayomugo2015@gmail.com>
2021-06-11 14:50:29 +0300

    Update on the building app Home file - upload Documents - view approval progress

---
+++ app/src/main/java/com/project/buildingapp/DocumentsFragment.java
@@ -0,0 +1,24 @@
+package com.project.buildingapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DocumentsFragment extends Fragment {
+
+    // TODO: Two versions view/edit and view only
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_documents, container, false);
+    }
+}
--- app/src/main/java/com/project/buildingapp/fragments/HomeFragment.java
+++ app/src/main/java/com/project/buildingapp/fragments/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,14 +12,54 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.project.buildingapp.R;
+import com.shuhart.stepview.StepView;
 
 public class HomeFragment extends Fragment {
 
     private View view;
+    
+    private TextView tvBuildingName, tvBuildingLocation, tvBuildingApproved, tvDocumentProgress, tvApprovalProgress, tvApprovalView;
+    private Button btnUploadDocuments;
+    private StepView stepViewApproval;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        // set
+
+
+        // find view by id
+        tvBuildingName = view.findViewById(R.id.tv_buildingname);
+        tvBuildingLocation = view.findViewById(R.id.tv_buildinglocation);
+        tvBuildingApproved = view.findViewById(R.id.tv_buildingapproved);
+        tvDocumentProgress = view.findViewById(R.id.tv_documentsprogress);
+        tvApprovalProgress = view.findViewById(R.id.tv_approvalprogress);
+        tvApprovalView = view.findViewById(R.id.tv_approvalview);
+        btnUploadDocuments = view.findViewById(R.id.btn_upload_documents);
+        stepViewApproval = view.findViewById(R.id.stepview_approval);
+
+        // set / load data
+
+
+        // listeners
+        btnUploadDocuments.setOnClickListener(uploadListener);
+        tvApprovalView.setOnClickListener(approveListener);
+
         return view;
     }
+
+
+    private View.OnClickListener uploadListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
+
+    private View.OnClickListener approveListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 }
